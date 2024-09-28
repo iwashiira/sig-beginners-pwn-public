17c4b99ea48ba1e5b814206d96ee66b3a9b2b520
iwashiira <iwashiira@gmail.com>
2024-09-29 02:09:11 +0900

    update sed command

--- glibc/glibc-source-install-ubuntu2404.sh
+++ glibc/glibc-source-install-ubuntu2404.sh
@@ -1,4 +1,4 @@
 sudo cp /etc/apt/sources.list.d/ubuntu.sources /etc/apt/sources.list.d/ubuntu.sources~
-sudo sed -Ei 's/^# deb-src /deb-src /' /etc/apt/sources.list.d/ubuntu.sources
+sudo sed -Ei 's/^Types: deb$/Types: deb deb-src/' /etc/apt/sources.list.d/ubuntu.sources
 sudo apt update
 apt source libc6
